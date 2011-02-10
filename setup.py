b384d5a49423fcc8a7089f1532773ceaf736df7e
Gael Pasgrimaud <gael@gawel.org>
2011-02-10 05:23:53 +0100

    pure and simple GitRepository app

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(name='git_http_backend',
       packages=find_packages(),
       include_package_data=True,
       zip_safe=False,
-      install_requires=[],
+      install_requires=['Paste', 'WebOb'],
       tests_require=[],
       entry_points = """\
       """,
